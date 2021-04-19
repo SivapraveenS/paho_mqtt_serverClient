5c1681235d91c9bf13a0f980613a93780c8f5cb7
Ubuntu <ubuntu@ip-172-31-36-240.us-east-2.compute.internal>
2021-04-19 10:33:23 +0000

    static ip change

--- paho-mqtt-wisenseClient.py
+++ paho-mqtt-wisenseClient.py
@@ -1,7 +1,3 @@
-
-"""
-Python MQTT Subscription client
-"""
 import paho.mqtt.client as mqtt
 import json
 
@@ -9,18 +5,23 @@ import json
 mqtt_username = "wisense"
 mqtt_password = "Wisense@123"
 mqtt_topic = "wsn_get_node_access"
-mqtt_broker_ip = "3.21.126.253"
+mqtt_broker_ip = "18.217.221.246"				#aws ec2 instance static ip, unless the instances wont gets shutdown and restarts again
+
+mqtt_response_topic = "wsn_node_access_response"
 
 verbose = 1
 
 client = mqtt.Client()
 client.username_pw_set(mqtt_username, mqtt_password)
 
-node_macId_1 = "fc:c2:3d:ff:fe:d:4a:46";
-node_macId_2 = "fc:c2:3d:ff:fe:d:5f:7c";
+node_macId_1 = "fc:c2:3d:ff:fe:0d:4a:46";
+node_macId_2 = "fc:c2:3d:ff:fe:0d:5f:7c";
+node_macId_3 = "fc:c2:3d:00:00:00:69:1c";			#mlitson demo SN-egg
 
 nodeMacId1RespMsg = {"macId":"fc:c2:3d:ff:fe:0d:4a:46", "username":"p0lsPBWqHD2AWhmKUlEu"}
-nodeMacId2RespMsg = {"macId":"fc:C2:3d:ff:fe:0d:5f:7c", "username":"st8T3pTKQ2LUBWBhCrzk"}
+nodeMacId2RespMsg = {"macId":"fc:c2:3d:ff:fe:0d:5f:7c", "username":"st8T3pTKQ2LUBWBhCrzk"}
+nodeMacId3RespMsg = {"macId":"fc:c2:3d:00:00:00:69:1c", "username":"hWeD9zf8oWnCx4LUxgXr"}
+
 
 if verbose == 1:
 	print("*****************************************************************************************************")
@@ -28,19 +29,22 @@ if verbose == 1:
 	print("nodeMacId1RespMsg macId: ", nodeMacId1RespMsg["macId"], "username: ", nodeMacId1RespMsg["username"])
 	print("nodeMacId2RespMsg type : ", type(nodeMacId2RespMsg))
 	print("nodeMacId2RespMsg macId: ", nodeMacId2RespMsg["macId"], "username: ", nodeMacId2RespMsg["username"])
+	print("nodeMacId3RespMsg type : ", type(nodeMacId3RespMsg))
+	print("nodeMacId3RespMsg macId: ", nodeMacId3RespMsg["macId"], "username: ", nodeMacId3RespMsg["username"])
 	print("Converting to MSG String Type...")
 
 nodeMacId1RespMsgDump = json.dumps(nodeMacId1RespMsg)
 nodeMacId2RespMsgDump = json.dumps(nodeMacId2RespMsg)
+nodeMacId3RespMsgDump = json.dumps(nodeMacId3RespMsg)
 
 if verbose == 1:
 	print("nodeMacId1RespMsgDump type: ", type(nodeMacId1RespMsgDump))
 	print("nodeMacId1RespMsgDUmp Content: ", nodeMacId1RespMsgDump)
 	print("nodeMacId2RespMsgDump type: ", type(nodeMacId2RespMsgDump))
 	print("nodeMacId2RespMsgDump Content: ", nodeMacId2RespMsgDump)
+	print("nodeMacId3RespMsgDump type: ", type(nodeMacId3RespMsgDump))
+	print("nodeMacId3RespMsgDump Content: ", nodeMacId3RespMsgDump)
 	print("*****************************************************************************************************")
-
-
 
 def on_connect(client, userdata, flags, rc):
     # rc - error code
@@ -53,10 +57,14 @@ def on_message(client, userdata, msg):
     print "Topic: ", msg.topic + "\nMessage: " + str(msg.payload)
     if node_macId_1 == msg.payload:
         print("Sending Node access response to wisense coord client !!")
-        client.publish("wsn_node_access_response", nodeMacId1RespMsgDump)
+        client.publish(mqtt_response_topic, nodeMacId1RespMsgDump)
     if node_macId_2 == msg.payload:
         print("Sending Node access response to wisense coord client !!")
-        client.publish("wsn_node_access_response", nodeMacId2RespMsgDump)
+        client.publish(mqtt_response_topic, nodeMacId2RespMsgDump)
+    if node_macId_3 == msg.payload:
+	print("Sending Node access response to wisense coord client !!")
+	client.publish(mqtt_response_topic, nodeMacId3RespMsgDump)
+
 
 client.on_connect = on_connect
 client.on_message = on_message
